f16d132b1f4e60421ecd27d108f983294595bf29
Nayma Garcia <nayma802@gmail.com>
2023-09-08 12:45:33 -0500

    playing with discover page UI

--- Experiments/naymag/Exp3/ui_experiments/testingUI/app/src/main/java/com/example/as1/CounterActivity.java
+++ Experiments/naymag/Exp3/ui_experiments/testingUI/app/src/main/java/com/example/as1/CounterActivity.java
@@ -1,48 +1,27 @@
 package com.example.as1;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.viewpager2.widget.ViewPager2;
 
 public class CounterActivity extends AppCompatActivity {
-
-    Button increaseBtn;
-    Button backBtn;
-    TextView numberTxt;
-
-    int counter = 0;
+    private CardView roundedCard;
+    private ImageView cardImage; // Add ImageView reference
+    private ViewPager2 viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_counter);
 
-        increaseBtn = findViewById(R.id.increaseBtn);
-        backBtn = findViewById(R.id.backBtn);
-        numberTxt = findViewById(R.id.number);
+        // Initialize your variables by finding the corresponding XML elements
+        roundedCard = findViewById(R.id.roundedCard);
+        cardImage = findViewById(R.id.cardImage); // Initialize the ImageView
+        viewPager = findViewById(R.id.viewPager);
 
-        increaseBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v)
-            {
-                numberTxt.setText(String.valueOf(++counter));
-            }
-        });
-
-
-        backBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v)
-            {
-                Intent intent = new Intent(CounterActivity.this, MainActivity.class);
-                startActivity(intent);
-            }
-        });
-
-
+        // You can now manipulate these elements in your code as needed
     }
 }
