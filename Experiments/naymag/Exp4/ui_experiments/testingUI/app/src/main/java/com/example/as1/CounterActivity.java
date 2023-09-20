371fb475a1922d886daeb68353f72219db13e247
Nayma Garcia <nayma802@gmail.com>
2023-09-20 08:41:17 -0500

    experimenting with login

---
+++ Experiments/naymag/Exp4/ui_experiments/testingUI/app/src/main/java/com/example/as1/CounterActivity.java
@@ -0,0 +1,55 @@
+package com.example.as1;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+public class CounterActivity extends AppCompatActivity {
+    private CardView roundedCard;
+    private ImageView cardImage;
+    private TextView textViewForever;
+    private TextView textViewNightTapes;
+    private ImageButton playButton;
+    private ImageButton pauseButton;
+    private MediaPlayer mediaPlayer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_counter);
+
+        // Initialize your variables by finding the corresponding XML elements
+        roundedCard = findViewById(R.id.roundedCard);
+        cardImage = findViewById(R.id.cardImage);
+        textViewForever = findViewById(R.id.textViewForever);
+        textViewNightTapes = findViewById(R.id.textViewNightTapes);
+        playButton = findViewById(R.id.playButton);
+        pauseButton = findViewById(R.id.pauseButton);
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.nightapes);
+
+    }
+
+    public void startMusic(View v){
+        mediaPlayer.start();
+    }
+
+    public void stopMusic(View v){
+        mediaPlayer.stop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mediaPlayer != null) {
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
+    }
+}
