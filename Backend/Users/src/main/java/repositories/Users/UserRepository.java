3bfc52067cbe545df13ab4d3e5f0c021ace5f4ee
QusaiEl <qusaielwa@gmail.com>
2023-09-25 17:50:10 -0500

    edit

---
+++ Backend/Users/src/main/java/repositories/Users/UserRepository.java
@@ -0,0 +1,13 @@
+package repositories.Users;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findById(int id);
+    void deleteById(int id);
+
+    boolean existsById(int id);
+
+}
