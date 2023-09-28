db7176b5135327a3a4f308081fe500a7452b3f34
Justin Soberano <jsoberano2002@gmail.com>
2023-09-28 06:54:02 -0500

    Changed directory location.

--- Backend/Users/src/main/java/mappers/Artist/Artist.java
+++ Backend/Users/src/main/java/mappers/Artist/Artist.java
@@ -9,15 +9,7 @@ public class Artist {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getId() {
         return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
     }
 }
