e85aa8fe59c6c3abd1a7c25eff17889442d9644f
Justin Soberano <jsoberano2002@gmail.com>
2023-09-28 06:54:51 -0500

    Changed directory location.

--- Backend/Users/src/main/java/mappers/Artist/ArtistMapper.java
+++ Backend/Users/src/main/java/mappers/Artist/ArtistMapper.java
@@ -2,7 +2,6 @@ package mappers.Artist;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
-import mappers.Artist.Artist;
 
 import java.lang.reflect.Type;
 import java.util.List;
