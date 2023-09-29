461585fa679376a55350aa3061f3933797ae45e3
Justin Soberano <jsoberano2002@gmail.com>
2023-09-29 12:36:18 -0500

    test

--- Backend/Users/src/main/java/mappers/Artist/Artist.java
+++ Backend/Users/src/main/java/mappers/Artist/Artist.java
@@ -1,14 +1,28 @@
 package mappers.Artist;
 
+/**
+ * Artist class for mapper
+ */
 public class Artist {
 
+    /* Artist ID */
     private String id;
+
+    /* Artist name */
     private String name;
 
+    /**
+     * Gets the name of the artist
+     * @return name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Gets the name of the ID
+     * @return id;
+     */
     public String getId() {
         return id;
     }
