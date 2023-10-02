9bc5372f778fe1a0f2e788125e1d6134bec825b5
QusaiEl <qusaielwa@gmail.com>
2023-10-02 18:18:44 -0500

    documented user controller

--- Backend/Users/src/main/java/userData/users/UserController.java
+++ Backend/Users/src/main/java/userData/users/UserController.java
@@ -13,6 +13,9 @@ import userData.trackCreation.Track.Track;
 import static userData.trackCreation.Spotify.SpotifyController.searchTrack;
 //import static userData.trackCreation.Spotify.SpotifyController.searchTrack;
 
+/**
+ * Controller used for all user action requests
+ */
 @RestController
 public class UserController {
 
@@ -25,11 +28,19 @@ public class UserController {
     @Autowired
     TrackRepository trackRepository;
 
+    /**
+     * @return list of all users in database
+     */
     @GetMapping(path = "/user")
     List<User> getAllUsers(){
         return userRepository.findAll();
     }
 
+    /**
+     *
+     * @param id, the id of the user
+     * @return json data of user of id
+     */
     @GetMapping(path = "/user/{id}")
     User getUserById( @PathVariable int id){
         if(userRepository.existsById(id)){
@@ -40,6 +51,11 @@ public class UserController {
 
     }
 
+    /**
+     * Post mapping for creating new users.
+     * @param user, request body(json) used for user creation
+     * @return String status
+     */
     @PostMapping(path = "/user")
     String createUser(User user){
         if (user == null) {
@@ -49,6 +65,12 @@ public class UserController {
         return "success";
     }
 
+    /**
+     * Request used to create a new station.
+     *
+     * @param id id of user
+     * @param id2 id of new station of user
+     */
     @PostMapping(path = "/user/{id}/station/{id2}")
     void createStation(@PathVariable int id, @PathVariable int id2){
         if(stationRepository.existsById(id2)) {
@@ -60,6 +82,12 @@ public class UserController {
         userRepository.findById(id).setStation(stationRepository.findById(id2));
     }
 
+    /**
+     * Seed setting request for rec generation
+     *
+     * @param id of user refered to.
+     * @param song string to be used in seeding the station
+     */
     @PutMapping(path = "user/{id}/station/{song}")
     void setStationSeed(@PathVariable int id, @PathVariable String song){
         Track t;
@@ -73,21 +101,44 @@ public class UserController {
         userRepository.save(u);
     }
 
+    /**
+     * Request for getting users station
+     *
+     * @param id of user refered to.
+     * @return station of user
+     */
     @GetMapping(path = "user/{id}/station")
     Station getStation(@PathVariable int id){
         return userRepository.findById(id).getStation();
     }
 
+    /**
+     * Request for getting list of generated tracks from a station
+     *
+     * @param id user whose station it is
+     * @param stationId station of user
+     * @return generated songs from station with seed
+     */
     @GetMapping(path = "user/{id}/{stationId}")
     List<Track> getTracks(@PathVariable int id, @PathVariable int stationId){
         return userRepository.findById(id).getStation().generateTacks();
     }
 
+    /**
+     *
+     * @param id
+     * @return
+     */
     @GetMapping(path = "user/{id}/profile")
     String getBio(@PathVariable int id){
         return userRepository.findById(id).getProfileStr();
     }
 
+    /**
+     *
+     * @param id
+     * @param string
+     */
     @PutMapping(path = "user/{id}/profile/{string}")
     void setBio(@PathVariable int id, @PathVariable String string){
         User u = userRepository.findById(id);
@@ -95,6 +146,11 @@ public class UserController {
         userRepository.save(u);
     }
 
+    /**
+     *
+     * @param id
+     * @return
+     */
     @DeleteMapping(path = "/user/{id}")
     String deleteUser(@PathVariable int id){
         if(userRepository.existsById(id)){
