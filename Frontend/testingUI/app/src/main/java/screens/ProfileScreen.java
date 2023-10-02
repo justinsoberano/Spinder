270aec2f3536c7976fc0cc99837498ecacda1a92
Nayma Garcia <nayma802@gmail.com>
2023-10-02 15:57:35 -0500

    pls work imoved to front

---
+++ Frontend/testingUI/app/src/main/java/screens/ProfileScreen.java
@@ -0,0 +1,35 @@
+package com.example.screens;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.as1.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class ProfileScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+        navBar();
+    }
+
+    private void navBar(){
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if(item.getItemId() == R.id.menu_discover){
+                    Intent discoverIntent = new Intent(ProfileScreen.this, MusicSwipe.class);
+                    startActivity(discoverIntent);
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
+
+}
