bb189c86a25bdd10e0db32c520a3e6d27f22aca6
Nayma Garcia <nayma802@gmail.com>
2023-10-17 16:52:46 -0500

    Added Stations Page (WIP)

---
+++ Frontend/testingUI/app/src/main/java/screens/Stations.java
@@ -0,0 +1,15 @@
+package screens;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.as1.R;
+
+public class Stations extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stations);
+    }
+}
