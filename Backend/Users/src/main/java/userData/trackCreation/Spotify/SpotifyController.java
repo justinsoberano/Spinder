ac15c34ed17e888d2fc10c2e710951e3d43445ea
Justin Soberano <jsoberano2002@gmail.com>
2023-10-17 13:31:05 -0500

    Added popularity.

--- Backend/Users/src/main/java/userData/trackCreation/Spotify/SpotifyController.java
+++ Backend/Users/src/main/java/userData/trackCreation/Spotify/SpotifyController.java
@@ -66,7 +66,10 @@ public class SpotifyController {
     }
 
 
-    public static List<Track> getRecommendations(List<String> seeds, int numSongs) {
+    public static List<Track> getRecommendations(List<String> seeds, int numSongs/*, int popularity*/) {
+
+        /* int max = Math.min(popularity + 20, 100);
+        int min = Math.max(popularity - 20, 0); */
 
         final GetRecommendationsRequest request = spotifyAPI.getRecommendations()
                 .limit(numSongs)
@@ -75,6 +78,8 @@ public class SpotifyController {
                 .seed_tracks(seeds.get(2))
                 .seed_tracks(seeds.get(3))
                 .seed_tracks(seeds.get(4))
+                // .max_popularity(max)
+                // .min_popularity(min)
                 .build();
 
         try {
@@ -89,7 +94,10 @@ public class SpotifyController {
         return null;
     }
 
-    public static List<Track> getRecommendationsByArtist(List<String> seeds, int numSongs) {
+    public static List<Track> getRecommendationsByArtist(List<String> seeds, int numSongs/*, int popularity*/) {
+
+        /* int max = Math.min(popularity + 20, 100);
+        int min = Math.max(popularity - 20, 0); */
 
         final GetRecommendationsRequest request = spotifyAPI.getRecommendations()
                 .limit(numSongs)
@@ -98,6 +106,8 @@ public class SpotifyController {
                 .seed_artists(seeds.get(2))
                 .seed_artists(seeds.get(3))
                 .seed_artists(seeds.get(4))
+                // .max_popularity(max)
+                // .min_popularity(min)
                 .build();
 
         try {
@@ -112,7 +122,10 @@ public class SpotifyController {
         return null;
     }
 
-    public static List<Track> getRecommendationsByGenre(List<String> seeds, int numSongs) {
+    public static List<Track> getRecommendationsByGenre(List<String> seeds, int numSongs/*, int popularity*/) {
+
+        /* int max = Math.min(popularity + 20, 100);
+        int min = Math.max(popularity - 20, 0); */
 
         final GetRecommendationsRequest request = spotifyAPI.getRecommendations()
                 .limit(numSongs)
@@ -121,6 +134,8 @@ public class SpotifyController {
                 .seed_genres(seeds.get(2))
                 .seed_genres(seeds.get(3))
                 .seed_genres(seeds.get(4))
+                // .max_popularity(max)
+                // .min_popularity(min)
                 .build();
 
         try {
