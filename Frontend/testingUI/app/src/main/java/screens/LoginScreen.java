3a838d3013e902b83aade0bd7987bb6f6b02c6c0
Nayma Garcia <nayma802@gmail.com>
2023-10-20 17:27:35 -0500

    sending login (WIP)

--- Frontend/testingUI/app/src/main/java/screens/LoginScreen.java
+++ Frontend/testingUI/app/src/main/java/screens/LoginScreen.java
@@ -3,19 +3,20 @@ package screens;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.graphics.drawable.AnimationDrawable;
-import android.graphics.drawable.GradientDrawable;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.as1.R;
+
+import org.json.JSONObject;
 
 import java.util.HashMap;
 
@@ -23,11 +24,12 @@ public class LoginScreen extends AppCompatActivity {
     EditText username;
     EditText password;
     Button loginButton;
-    ImageView logoimg;
     LinearLayout logoRelative;
+    Button registerButton;
+    String baseUrl = "http://10.0.2.2:8080/";
+
 
     private HashMap<String, String> userCredentials;
-    private GradientDrawable gradientDrawable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,15 +39,15 @@ public class LoginScreen extends AppCompatActivity {
         password = findViewById(R.id.password);
         loginButton = findViewById(R.id.loginButton);
         logoRelative = findViewById(R.id.logoRelative);
+        registerButton = findViewById(R.id.signUpButton);
         userCredentials = new HashMap<>();
         userCredentials.put("username", "password");
+        String enteredUsername = username.getText().toString();
+        String enteredPassword = password.getText().toString();
 
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String enteredUsername = username.getText().toString();
-                String enteredPassword = password.getText().toString();
-
                 if (validateLogin(enteredUsername, enteredPassword)) {
                     Intent intent = new Intent(LoginScreen.this, SeedSetter.class);
                     startActivity(intent);
@@ -54,6 +56,45 @@ public class LoginScreen extends AppCompatActivity {
                 }
             }
         });
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                registerButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                        String usernameSend = enteredUsername;
+                        String passwordSend = enteredPassword;
+
+                        if (!usernameSend.isEmpty() && !passwordSend.isEmpty()) {
+                            String url = baseUrl + "user/" + usernameSend + "/" + passwordSend;
+
+                            JSONObject requestBody = new JSONObject();
+
+                            JsonObjectRequest request = new JsonObjectRequest(
+                                    Request.Method.PUT,
+                                    url,
+                                    requestBody,
+                                    null,
+                                    null
+                            );
+
+                            Volley.newRequestQueue(LoginScreen.this).add(request);
+
+                            Toast.makeText(LoginScreen.this, "Registered Successful", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(LoginScreen.this, "Please Register", Toast.LENGTH_SHORT).show();
+                        }
+
+                        Uri uri = Uri.parse("http://10.0.2.2:8080/register");
+                        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                        startActivity(intent);
+                    }
+                });
+            }
+        });
+
     }
 
     public boolean validateLogin(String enteredUsername, String enteredPassword) {
