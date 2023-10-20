1b5a0680cf4b011f66a29402e607a3747c357b9e
Nayma Garcia <nayma802@gmail.com>
2023-10-20 14:19:23 -0500

    added button to add new station

--- Frontend/testingUI/app/src/main/java/screens/Stations.java
+++ Frontend/testingUI/app/src/main/java/screens/Stations.java
@@ -1,15 +1,19 @@
 package screens;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.as1.R;
 
 public class Stations extends AppCompatActivity {
+    ImageView newStation;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stations);
+        newStation = findViewById(R.id.newStation);
+
     }
 }
