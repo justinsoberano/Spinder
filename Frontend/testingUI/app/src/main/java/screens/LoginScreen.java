55b1afad5c530a2d6582f27325c78d03ed6348fe
naymag <naymag@iastate.edu>
2023-12-02 16:45:52 -0600

    swipe up stuff done

--- Frontend/testingUI/app/src/main/java/screens/LoginScreen.java
+++ Frontend/testingUI/app/src/main/java/screens/LoginScreen.java
@@ -36,7 +36,6 @@ public class LoginScreen extends AppCompatActivity {
     Button registerButton;
     String baseUrl = "http://coms-309-056.class.las.iastate.edu:8080/";
     String isUserValid = "false";
-
     private HashMap<String, String> userCredentials;
 
     @Override
@@ -59,15 +58,16 @@ public class LoginScreen extends AppCompatActivity {
                 String enteredPassword = password.getText().toString();
                 GlobalVariables.userName = enteredUsername;
 
-                    Intent intent = new Intent(LoginScreen.this, SeedSetter.class);
-                    startActivity(intent);
-
-//                if(validateLogin(enteredUsername, enteredPassword)){
+                //ONLY USE WHEN SERVER ISNT RUNNING AND YOU NEED TO GET INTO THE APP
 //                    Intent intent = new Intent(LoginScreen.this, SeedSetter.class);
 //                    startActivity(intent);
-//                }else{
-//                    Toast.makeText(LoginScreen.this, "User Not Found", Toast.LENGTH_SHORT).show();
-//                }
+
+                if(validateLogin(enteredUsername, enteredPassword)){
+                    Intent intent = new Intent(LoginScreen.this, SeedSetter.class);
+                    startActivity(intent);
+                }else{
+                    Toast.makeText(LoginScreen.this, "User Not Found", Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
@@ -96,6 +96,12 @@ public class LoginScreen extends AppCompatActivity {
                             Volley.newRequestQueue(LoginScreen.this).add(request);
 
                             Toast.makeText(LoginScreen.this, "Registered Successful", Toast.LENGTH_SHORT).show();
+
+                            String spotifyRedirect = "http://coms-309-056.class.las.iastate.edu:8080/register/" + usernameSend;
+                            Uri uri = Uri.parse(spotifyRedirect);
+                            Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                            startActivity(intent);
+
                         } else {
                             Toast.makeText(LoginScreen.this, "Please Register", Toast.LENGTH_SHORT).show();
                         }
--- Frontend/testingUI/app/src/main/java/screens/MusicSwipe.java
+++ Frontend/testingUI/app/src/main/java/screens/MusicSwipe.java
@@ -24,6 +24,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.palette.graphics.Palette;
 
 import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.example.as1.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.android. volley.Request;
@@ -49,6 +50,7 @@ public class MusicSwipe extends AppCompatActivity {
     private MediaPlayer mediaPlayer;
     private int currentSongIndex = 0;
     String baseUrl = "http://coms-309-056.class.las.iastate.edu:8080/";
+    private final ArrayList<String> songIds = new ArrayList<>();
     private final ArrayList<String> songNames = new ArrayList<>();
     private final ArrayList<String> artistNames = new ArrayList<>();
     private final ArrayList<String> songImages = new ArrayList<>();
@@ -73,7 +75,6 @@ public class MusicSwipe extends AppCompatActivity {
     }
 
 
-
     private void setUpNavBar(){
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -94,9 +95,8 @@ public class MusicSwipe extends AppCompatActivity {
         if(GlobalVariables.userName == null){
             return;
         }
-        String url = baseUrl + "user/" + GlobalVariables.userName + "/station";
-//        String url = "http://10.0.2.2:8080/user/1/1";
 
+        String url = baseUrl + "user/" + GlobalVariables.userName + "/station";
 
         JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(
                 Request.Method.GET,
@@ -109,12 +109,14 @@ public class MusicSwipe extends AppCompatActivity {
                         try {
                             for(int i = 0; i < response.length(); i++){
                                 JSONObject songObject = response.getJSONObject(i);
+                                String songId = songObject.getString("id");
                                 String songName = songObject.getString("name");
                                 String artistName = songObject.getString("artist");
                                 String songImage = songObject.getString("image");
                                 String songSnippet = songObject.getString("preview");
 
                                 if(!songSnippet.equals("null")){
+                                    songIds.add(songId);
                                     songNames.add(songName);
                                     artistNames.add(artistName);
                                     songImages.add(songImage);
@@ -151,6 +153,21 @@ public class MusicSwipe extends AppCompatActivity {
         requestQueue.add(jsonArrayRequest);
     }
 
+    private void addSongToPlaylist(String songID){
+        String url = baseUrl + "add/" + GlobalVariables.userName + "/" + songID;
+
+        JSONObject requestBody = new JSONObject();
+
+        JsonObjectRequest request = new JsonObjectRequest(
+                Request.Method.PUT,
+                url,
+                requestBody,
+                null,
+                null
+        );
+
+        Volley.newRequestQueue(MusicSwipe.this).add(request);
+    }
 
     private class SwipeListener implements View.OnTouchListener {
         GestureDetector gestureDetector;
@@ -181,7 +198,7 @@ public class MusicSwipe extends AppCompatActivity {
                                     currentSongIndex = (currentSongIndex + 1) % songImages.size();
                                 } else {
                                     currentSongIndex = (currentSongIndex - 1 + songImages.size()) % songImages.size();
-                                    // change info to say "you did not like this song"
+                                    // Goes Back to the previous song
                                 }
 
                                 if (mediaPlayer != null) {
@@ -199,8 +216,11 @@ public class MusicSwipe extends AppCompatActivity {
                         } else {
                             if (absY > threshold && Math.abs(velocityY) > velocityThreshold) {
                                 if (yDif < 0) {
-                                    // Perform action when swiped up
-                                    showToast("Swiped up");
+                                    //adds song to playlist in their spotify account
+                                    String currentSongId;
+                                    currentSongId = songIds.get(currentSongIndex);
+                                    addSongToPlaylist(currentSongId);
+                                    showToast("Song Added to Playlist");
                                     return true;
                                 }
                             }
