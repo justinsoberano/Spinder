6a5fec1c2226f930721fb088cc195fbb39102a6e
justinsoberano <jus@iastate.edu>
2024-03-16 08:21:14 -0500

    Delete Experiments directory

--- Experiments/naymag/Exp2/tutorials-android_unit2_1_volley/AndroidVolley/app/src/main/java/com/example/sumon/androidvolley/utils/Const.java
+++
@@ -1,8 +0,0 @@
-package com.example.sumon.androidvolley.utils;
-
-public class Const {
-	public static final String URL_JSON_OBJECT = "https://jsonplaceholder.typicode.com/users/1";
-	public static final String URL_JSON_ARRAY = "https://jsonplaceholder.typicode.com/users";
-	public static final String URL_STRING_REQ = "https://jsonplaceholder.typicode.com/users/1";
-	public static final String URL_IMAGE = "https://upload.wikimedia.org/wikipedia/en/f/f2/Taylor_Swift_-_Reputation.png";
-}
